cbf065c85e2247e3c2ba7fa751dbb4e9396ec8c9
iskandarsofina@gmail.com <iskandarsofina@gmail.com>
2024-10-06 17:31:21 +0500

    last_version

--- computer/computer.gd
+++ computer/computer.gd
@@ -3,16 +3,15 @@ extends StaticBody2D
 @onready var pscene = load("res://scenes/algae_container_view.tscn")
 @onready var interaction_area = $Sprite2D/InteractionArea
 @onready var algae_container_view = $algae_container_view
+@onready var camera2 = $"../Camera2D2"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	algae_container_view.visible = false
 	interaction_area.interact = Callable(self, 'interact')
 	
 
 func interact():
-	#algae_container_view.visible = true
-	get_tree().change_scene_to_packed(pscene)
+	camera2.make_current()
 
 
 func _on_interaction_area_body_entered(body: Node2D) -> void:
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
-@export var movement_speed = 60
+@export var movement_speed = 80
 @export var hp = 1
-@onready var algae = get_tree().get_nodes_in_group('algae')
+@onready var dead_sound = get_node("Death")
+@onready var anim = get_node("AnimationPlayer")
+@onready var algae
 var closest_distance
 var closest_target
 var mouse_in := false
 var pressed := false
 
+func die():
+	if pressed == true:
+		print('yeah')
+		dead_sound.play()
+		return true
 
 func _physics_process(delta: float):
-	print(mouse_in)
+	algae = get_tree().get_nodes_in_group("algae")
 	var enemy_position = global_position
 	closest_distance = enemy_position.distance_to(algae[0].global_position)
 	closest_target = algae[0].global_position
@@ -19,11 +26,20 @@ func _physics_process(delta: float):
 			closest_target = alga.global_position
 	var direction = global_position.direction_to(closest_target)
 	velocity = direction*movement_speed
-	if pressed == true:
-		queue_free()
+	if self.has_method('die'):
+		die()
 	move_and_slide()
 
-func _input(event):
+
+	
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_viewport_rect().has_point(to_local(event.position)):
 			pressed = true
+			print('bruh')
+
+
+
+
+func _on_death_finished() -> void:
+	queue_free()
